a15f19a9e6abd93a5918752cfeb0ece8f1bfb9c1
Dennis Kaselow <Dnnis.Kaselow+github@gmail.com>
2013-03-31 10:42:20 +0200

    finished palette-example

--- example/web/src/palette.dart
+++ example/web/src/palette.dart
@@ -3,83 +3,66 @@ part of examples;
 var dawnpaletteCq;
 var nespaletteCq;
 
-var paletteData = {'dawnbringer': {'url': 'http://www.pixeljoint.com/forum/forum_posts.asp?TID=12795', 'factor': 16},
-               'nes': {'url': 'http://en.wikipedia.org/wiki/File:NES_palette.png'}, 'factor': 8};
+var paletteData = {'dawnbringer': {'name': 'Dawnbringer', 'res': 'dawnbringerpalette.png', 'url': 'http://www.pixeljoint.com/forum/forum_posts.asp?TID=12795'},
+               'nes': {'name': 'NES', 'res': 'nespalette.png', 'url': 'http://en.wikipedia.org/wiki/File:NES_palette.png'},
+               'pal': {'name': 'PAL', 'res': 'palpalette.png', 'url': 'http://en.wikipedia.org/wiki/File:Atari2600_PAL_palette.png'},
+               'sega': {'name': 'SEGA', 'res': 'mastersystempalette.png', 'url': 'http://en.wikipedia.org/wiki/File:RGB_6bits_palette.png'}};
+
+var imageData = {'dawnbringer' : {'name': 'Dawnbringer', 'res': 'dawnbringermockup.png', 'url': 'http://www.pixeljoint.com/forum/forum_posts.asp?TID=12795'},
+                 'farming': {'name': 'Farming', 'res': 'farminglpc.png', 'url': 'http://opengameart.org/content/farming-tilesets-magic-animations-and-ui-elements'}};
 
 void palette(DivElement parent) {
-  Future.wait([loadImage('dawnbringerpalette.png'), loadImage('nespalette.png'), loadImage('farminglpc.png')]).then((images) {
-    paletteData['dawnbringer']['img'] = images[0];
-    paletteData['nes']['img'] = images[1];
-    var farmingImg = images[2];
-    var farmingCq = cq(farmingImg);
-    farmingCq..appendTo(parent);
-
-    SelectElement select = query('#paletteselect');
-    select.queryAll('option').forEach((option) {
-      option.onMouseOver.listen((e) {
-        showPalette(e, cq(paletteData[option.value]['img'])..resizePixel(paletteData[option.value]['factor']));
-      });
-    });
-    select.onChange.listen((_) {
-      var selection = select.value;
-      if ("" == selection) {
-        farmingCq.replaceWith(cq(farmingImg));
+  SelectElement paletteSelect = query('#paletteselect');
+  SelectElement imageSelect = query('#imageselect');
+  paletteSelect.append(new OptionElement('None', '', true, true));
+  
+  var loader = new List<Future<ImageElement>>();
+  loader.addAll(initDropdown(paletteSelect, paletteData));
+  loader.addAll(initDropdown(imageSelect, imageData));
+  
+  Future.wait(loader).then((_) {
+    var paletteInfo = new DivElement();
+    var link = new AnchorElement();
+    paletteInfo..classes.add('credits')
+               ..appendText('Palette: ')
+               ..append(link);
+    
+    var currentCq = cq(imageData['dawnbringer']['img']);
+    currentCq..appendTo(parent);
+    
+    void updateExample(_) {
+      var palleteSelection = paletteSelect.value;
+      var selectedImage = imageData[imageSelect.value]['img']; 
+      var nextCq = cq(selectedImage);      
+      if ("" == palleteSelection) {
+        currentCq.replaceWith(nextCq);
+        paletteInfo.remove();
       } else {
-        var palette = cq(paletteData[selection]['img']).getPalette();
-        farmingCq.replaceWith(cq(farmingImg)..matchPalette(palette));
+        var palette = cq(paletteData[palleteSelection]['img']).getPalette();
+        paletteInfo.style.width = '${selectedImage.width}px';
+        currentCq.replaceWith(nextCq..matchPalette(palette)..canvas.title = '.matchPalette(palette)');
+        link.href = paletteData[palleteSelection]['url'];
+        link.text = '${paletteData[palleteSelection]['name']}';
+        parent.append(paletteInfo);
       }
-    });
-
-//    var dawnpalette = cq(dawnpaletteImg).getPalette();
-//    var nespalette = cq(nespaletteImg).getPalette();
-//    dawnpaletteCq = cq(dawnpaletteImg)..resizePixel(16);
-//    nespaletteCq = cq(nespaletteImg)..resizePixel(8);
-//    DivElement container = new DivElement();
-//    container.style.width = '300px';
-//    cq(image)..matchPalette(dawnpalette)
-//             ..canvas.onMouseMove.listen(showDawnPalette)
-//             ..canvas.onMouseOut.listen(hidePalette)
-//             ..drawImage(dawnpaletteCq.canvas, 0, 0)
-//             ..appendTo(container);
-//    DivElement credits = new DivElement();
-//    credits.classes.add('credits');
-//    credits.appendHtml('''
-//<a href="http://www.pixeljoint.com/forum/forum_posts.asp?TID=12795">DawnBringer's palette</a>
-//''');
-//    container.append(credits);
-//    parent.append(container);
-//    cq(image)..matchPalette(nespalette)
-//             ..canvas.onMouseMove.listen(showNesPalette)
-//             ..canvas.onMouseOut.listen(hidePalette)
-//             ..drawImage(nespaletteCq.canvas, 0, 0)
-//             ..appendTo(parent);
-  });
-}
-
-void showDawnPalette(MouseEvent e) {
-  showPalette(e, dawnpaletteCq.canvas);
-}
-
-void showNesPalette(MouseEvent e) {
-  showPalette(e, nespaletteCq.canvas);
-}
-
-void showPalette(MouseEvent e, CanvasQuery palette) {
-  int x = e.client.x;
-  int y = e.client.y;
-  DivElement div = query('#tooltip');
-  var canvas = div.query('canvas');
-  if (null == canvas || canvas != palette.canvas) {
-    if (null != canvas) {
-      canvas.remove();
     }
-  }
-  div..style.display = 'block'
-     ..style.top = '${y}px'
-     ..style.left = '${x}px'
-     ..append(palette.canvas);
+    
+    paletteSelect.onChange.listen(updateExample);
+    imageSelect.onChange.listen(updateExample);
+  });
+
 }
 
-void hidePalette(MouseEvent e) {
-  query('#tooltip').style.display = 'none';
+initDropdown(SelectElement dropdown, Map<String, Map<String, dynamic>> data) {
+  var futures = new List<Future<ImageElement>>();
+  data.forEach((key, item) {
+    var completer = new Completer();
+    futures.add(completer.future);
+    loadImage(item['res']).then((image) {
+      item['img'] = image;
+      dropdown.append(new OptionElement(item['name'], key, false, false));
+      completer.complete(image);
+    });
+  });
+  return futures;
 }
